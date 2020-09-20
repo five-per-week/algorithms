c243cc0e8a3af8724318e2d2e9a75bc61acf7fb5
younho9 <younho9.choo@gmail.com>
2020-09-20 17:17:14 +0900

    chore: Setup problem

---
+++ problems/programmers/42576-an-uncompleted-player/younho9.js
@@ -0,0 +1,6 @@
+function solution(participant, completion) {
+    var answer = '';
+    return answer;
+}
+
+module.exports = solution;
---
+++ problems/programmers/42576-an-uncompleted-player/younho9.test.js
@@ -0,0 +1,22 @@
+const solution = require('./younho9');
+
+describe('1번', () => {
+    it('test 1', () => {
+        const participant = ['leo', 'kiki', 'eden'];
+        const completion = ['eden', 'kiki'];
+        const result = 'leo';
+        expect(solution(participant, completion)).toEqual(result);
+    });
+    it('test 2', () => {
+        const participant = ['marina', 'josipa', 'nikola', 'vinko', 'filipa'];
+        const completion = ['josipa', 'filipa', 'marina', 'nikola'];
+        const result = 'vinko';
+        expect(solution(participant, completion)).toEqual(result);
+    });
+    it('test 3', () => {
+        const participant = ['mislav', 'stanko', 'mislav', 'ana'];
+        const completion = ['stanko', 'ana', 'mislav'];
+        const result = 'mislav';
+        expect(solution(participant, completion)).toEqual(result);
+    });
+});
