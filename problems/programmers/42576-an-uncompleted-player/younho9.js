1df014eefbe8723b754372963cde61980c9fcfac
younho9 <younho9.choo@gmail.com>
2020-09-21 10:56:40 +0900

    ✅ younho9: solved 완주하지 못한 선수

--- problems/programmers/42576-an-uncompleted-player/younho9.js
+++ problems/programmers/42576-an-uncompleted-player/younho9.js
@@ -1,5 +1,17 @@
 function solution(participant, completion) {
-    var answer = '';
+    const hashMap = completion.reduce((acc, cur) => {
+        acc.has(cur) ? acc.set(cur, acc.get(cur) + 1) : acc.set(cur, 1);
+        return acc;
+    }, new Map());
+
+    return participant.find((person) => {
+        if (hashMap.get(person)) {
+            hashMap.set(person, hashMap.get(person) - 1);
+        } else {
+            return true;
+        }
+    });
+
     return answer;
 }
 
