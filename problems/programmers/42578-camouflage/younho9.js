fe189edea9135618e4a4fcab177981f72eb18775
younho9 <younho9.choo@gmail.com>
2020-09-22 02:15:42 +0900

    refactor: Use Object instead of Map

--- problems/programmers/42578-camouflage/younho9.js
+++ problems/programmers/42578-camouflage/younho9.js
@@ -1,11 +1,9 @@
 const solution = (clothes) =>
-    [
-        ...clothes
-            .reduce(
-                (acc, cur) => acc.set(cur[1], (acc.get(cur[1]) || 1) + 1),
-                new Map(),
-            )
-            .values(),
-    ].reduce((acc, cur) => (acc === 0 ? acc + cur : acc * cur), 0) - 1;
+    Object.values(
+        clothes.reduce((acc, cur) => {
+            acc[cur[1]] = (acc[cur[1]] || 1) + 1;
+            return acc;
+        }, {}),
+    ).reduce((acc, cur) => (acc === 0 ? acc + cur : acc * cur), 0) - 1;
 
 module.exports = solution;
