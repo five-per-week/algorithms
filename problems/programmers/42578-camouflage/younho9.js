4e13854477e0c6f0499d6390a5466f594fa1b6b8
younho9 <younho9.choo@gmail.com>
2020-09-22 02:08:45 +0900

    refactor: Programmers 42578

--- problems/programmers/42578-camouflage/younho9.js
+++ problems/programmers/42578-camouflage/younho9.js
@@ -1,16 +1,11 @@
-function solution(clothes) {
-    const hashMap = clothes.reduce(
-        (acc, cur) => acc.set(cur[1], (acc.get(cur[1]) || 1) + 1),
-        new Map(),
-    );
-
-    let answer =
-        [...hashMap.values()].reduce(
-            (acc, cur, idx) => (idx === 0 ? acc + cur : acc * cur),
-            0,
-        ) - 1;
-
-    return answer;
-}
+const solution = (clothes) =>
+    [
+        ...clothes
+            .reduce(
+                (acc, cur) => acc.set(cur[1], (acc.get(cur[1]) || 1) + 1),
+                new Map(),
+            )
+            .values(),
+    ].reduce((acc, cur) => (acc === 0 ? acc + cur : acc * cur), 0) - 1;
 
 module.exports = solution;
