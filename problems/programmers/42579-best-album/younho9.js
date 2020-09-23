1c04c2a1d770262106276644ef0eb519a7129488
Younho Choo <younho9.choo@gmail.com>
2020-09-23 11:00:26 +0900

    chore: Setup problem

---
+++ problems/programmers/42579-best-album/younho9.js
@@ -0,0 +1,4 @@
+function solution(input) {
+    let answer = '';
+    return answer;
+}
---
+++ problems/programmers/42579-best-album/younho9.test.js
@@ -0,0 +1,9 @@
+const solution = require('./younho9');
+
+describe('42579-best-album', () => {
+    it('test 1', () => {
+        const input = '';
+        const result = '';
+        expect(solution(input)).toEqual(result);
+    });
+});
