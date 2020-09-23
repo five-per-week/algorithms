581753bc0847dc0aee6b7dba487c0d3ac02713dc
Younho Choo <younho9.choo@gmail.com>
2020-09-23 11:05:23 +0900

    test: Add test case

--- problems/programmers/42579-best-album/younho9.test.js
+++ problems/programmers/42579-best-album/younho9.test.js
@@ -2,8 +2,9 @@ const solution = require('./younho9');
 
 describe('42579-best-album', () => {
     it('test 1', () => {
-        const input = '';
-        const result = '';
-        expect(solution(input)).toEqual(result);
+        const genres = ['classic', 'pop', 'classic', 'classic', 'pop'];
+        const plays = [500, 600, 150, 800, 2500];
+        const result = [4, 1, 3, 0];
+        expect(solution(genres, plays)).toEqual(result);
     });
 });
