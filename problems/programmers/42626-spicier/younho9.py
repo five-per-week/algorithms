5647a84ddd1a94d185005b943a7db3b103696ee1
Younho Choo <younho9.choo@gmail.com>
2020-10-03 14:31:40 +0900

    ❌ unsolved: Programmers 42626 @younho9
    테스트 케이스 중 런타임 에러 발생

---
+++ problems/programmers/42626-spicier/younho9.py
@@ -0,0 +1,16 @@
+import heapq
+
+
+def solution(scoville, K):
+    heapq.heapify(scoville)
+    count = 0
+
+    while any(food < K for food in scoville):
+        heapq.heappush(scoville, heapq.heappop(scoville) + heapq.heappop(scoville) * 2)
+        count += 1
+
+    return count
+
+
+if __name__ == "__main__":
+    print(solution([1, 2, 3, 9, 10, 12], 7) == 2)
