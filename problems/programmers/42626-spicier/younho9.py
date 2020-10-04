e9f97814b4304b4f04f8ebdbea3bfadf0c4e02a6
Younho Choo <younho9.choo@gmail.com>
2020-10-04 21:17:55 +0900

    ✅ solved: Programmers 42626 @younho9 (#34)
    * ❌ unsolved: Programmers 42626 @younho9
    
    테스트 케이스 중 런타임 에러 발생
    
    * ✅ solved: Programmers 42626 @younho9
    
    * refactor: Programmers 42626
    
    * ✅ solved: Programmers 42626 JS @younho9

---
+++ problems/programmers/42626-spicier/younho9.py
@@ -0,0 +1,19 @@
+import heapq
+
+
+def solution(scoville, K):
+    heapq.heapify(scoville)
+    count = 0
+
+    while scoville[0] < K:
+        if len(scoville) < 2:
+            return -1
+
+        heapq.heappush(scoville, heapq.heappop(scoville) + heapq.heappop(scoville) * 2)
+        count += 1
+
+    return count
+
+
+if __name__ == "__main__":
+    print(solution([1, 2, 3, 9, 10, 12], 7) == 2)
